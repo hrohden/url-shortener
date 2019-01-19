bd7016179e53b7e7515b2142ca78b809c1ee232e
Henrique Broering Rohden <hrohden@gmail.com>
2019-01-18 22:33:49 -0200

    Move NGINX config file to config.d directory

--- app/src/main/java/com/henriquerohden/urlshortener/UrlShortenerApplication.java
+++ app/src/main/java/com/henriquerohden/urlshortener/UrlShortenerApplication.java
@@ -2,12 +2,22 @@ package com.henriquerohden.urlshortener;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+@Controller
 public class UrlShortenerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UrlShortenerApplication.class, args);
+	}
+
+	@GetMapping("/")
+	@ResponseBody
+	public String home() {
+		return "Hello, world!";
 	}
 
 }
