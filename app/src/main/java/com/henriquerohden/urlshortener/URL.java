7c8ba37d3a6dcbca15aa6059229c6829b49a073c
Henrique Broering Rohden <hrohden@gmail.com>
2019-01-19 17:30:08 -0200

    Add mock initial data

--- app/src/main/java/com/henriquerohden/urlshortener/URL.java
+++ app/src/main/java/com/henriquerohden/urlshortener/URL.java
@@ -4,22 +4,23 @@ import javax.persistence.*;
 import java.time.LocalDateTime;
 import lombok.Data;
 
+import static javax.persistence.GenerationType.IDENTITY;
+
 @Entity
 @Data
 @Table(name = "urls")
-public class URL {
+class URL {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = IDENTITY)
     private Long id;
 
     @Column(name = "original_url")
     private String originalURL;
 
-    @Column(name = "hash")
+    @Column(name = "hash_code")
     private String hash;
 
-    @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "creation_date")
     private LocalDateTime creationDate;
 }
