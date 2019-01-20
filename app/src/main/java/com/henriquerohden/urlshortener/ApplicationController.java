c26001bfd383f9f68d2556c4bb21efffdaa64100
Henrique Broering Rohden <hrohden@gmail.com>
2019-01-20 14:23:56 -0200

    Create API endpoint in order to retrieve statistics and other relevant data

--- app/src/main/java/com/henriquerohden/urlshortener/ApplicationController.java
+++ app/src/main/java/com/henriquerohden/urlshortener/ApplicationController.java
@@ -1,11 +1,9 @@
 package com.henriquerohden.urlshortener;
 
+import org.hibernate.stat.Statistics;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Arrays;
@@ -37,6 +35,12 @@ public class ApplicationController {
         url.addHits();
         repository.save(url);
         return "redirect:" + url.getOriginalURL();
+    }
+
+    @GetMapping(value = "/api/{hash}", produces = "application/json")
+    @ResponseBody
+    public URL statistics(@PathVariable("hash") String hash) {
+        return repository.findByHash(hash);
     }
 
 }
